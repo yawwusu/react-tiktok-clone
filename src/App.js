224d0d2540cea2a683beb0941b25e6cf14cfa1f2
yawwusu <yawwusugh@gmail.com>
2021-06-18 13:00:48 +0200

    Connected firebase db and made components dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import React from "react";
 import "./App.css";
+import { db } from "./firebase";
 import Video from "./Video";
 
 function App() {
+  const [videos, setVideos] = React.useState([]);
+
+  React.useEffect(() => {
+    const unsubscribe = db
+      .collection("videos")
+      .onSnapshot((snapshot) =>
+        setVideos(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
+      );
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <div className="App-videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        {videos.map((video) => (
+          <Video
+            key={video.id}
+            url={video.url}
+            channel={video.channel}
+            description={video.description}
+            song={video.song}
+            likes={video.likes}
+            messages={video.messages}
+            shares={video.shares}
+          />
+        ))}
       </div>
     </div>
   );
--- src/Video.js
+++ src/Video.js
@@ -3,7 +3,7 @@ import "./Video.css";
 import VideoFooter from "./VideoFooter";
 import VideoSidebar from "./VideoSidebar";
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
   const [playing, setPlaying] = React.useState(false);
   const videoRef = React.useRef(null);
 
@@ -24,14 +24,10 @@ function Video() {
         ref={videoRef}
         loop
         onClick={handleVideoPress}
-        src="https://v77.tiktokcdn.com/082b02f7ed89503e38447e01d6f4df68/60ccb6b3/video/tos/useast2a/tos-useast2a-ve-0068c004/b462a93ba59446a0b3c1a00b4d5f9103/?a=1233&br=2070&bt=1035&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210618090702010190185130200111C8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2g7dG01ajtlNjMzNzczM0ApaTdmOzhlNTtpN2k8OTo0OGdgLnFsazQvNTFgLS1kMTZzczMzLmE0YTYuLzQ2LS1hXl86Yw%3D%3D&vl=&vr="
+        src={url}
       ></video>
-      <VideoFooter
-        channel="yaw"
-        description="nyansani"
-        song="DJ Acouzo - song"
-      />
-      <VideoSidebar likes={200} messages={432} shares={303} />
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
     </div>
   );
 }
